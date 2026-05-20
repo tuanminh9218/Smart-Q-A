22d1cdb13deeaf6e52d59fd0cabecf3f1c4af36b
tuanminh9218 <tuanminh9218@gmail.com>
2026-05-20 09:26:54 +0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/Smart-Q-A/",
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
